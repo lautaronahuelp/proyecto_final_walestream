0c3a0b1035cf3eff072d6fb3abd73ff91f2ed1b4
Lautaro Nahuel <lautaro@outlook.com>
2021-01-06 14:54:12 -0300

    Desafio 5: correcciones menores.

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -9,11 +9,7 @@ const ItemListContainer = function(props){
             <h2>Próximos eventos</h2>
             <p>Conoce nuestros próximos eventos y compra tickets para tus preferidos.</p>
             <p>{props.eventosProximos}</p>
-            <ItemList items={[
-                    {id:1, title: "evento 1", description:"descripcion del evento uno", price:500, pictureUrl:"https://i.pinimg.com/originals/7c/06/8b/7c068be74941226b80161e8cc8dafa01.jpg"},
-                    {id:2, title: "evento 2", description:"descripcion del evento dos", price:500, pictureUrl:"https://i.pinimg.com/originals/7c/06/8b/7c068be74941226b80161e8cc8dafa01.jpg"},
-                    {id:3, title: "evento 3", description:"descripcion del evento tres", price:500, pictureUrl:"https://i.pinimg.com/originals/7c/06/8b/7c068be74941226b80161e8cc8dafa01.jpg"},
-                    {id:4, title: "evento 4", description:"descripcion del evento cuatro", price:500, pictureUrl:"https://i.pinimg.com/originals/7c/06/8b/7c068be74941226b80161e8cc8dafa01.jpg"}]}/>
+            <ItemList />
         </div>
         
     )
